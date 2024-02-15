93802580f6b6cc34e083489ee9a9fd461b893589
Łukasz Smyrak <lsmyrak@gmail.com>
2024-02-15 07:27:20 +0100

    Added some code in viewmodel process

--- InjectorDll/ViewModels/ProcessListViewModel.cs
+++ InjectorDll/ViewModels/ProcessListViewModel.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace InjectorDll.ViewModels
 {
-    public class ProcessListViewModel :ViewModelBase
+    public class ProcessListViewModel : ViewModelBase
     {
+        private ObservableCollection<ProcessIdNameViewModel> _processIdNameViewModels;
+
+        private ICommand _cmdRefreshProcessList;
+
+        public ObservableCollection<ProcessIdNameViewModel> ProcessIdNameViewModels
+        {
+            get
+            {
+                return _processIdNameViewModels;
+            }
+            set
+            {
+                _processIdNameViewModels = value;
+                OnPropertyChanged(() => ProcessIdNameViewModels);
+            }
+        }
+
+        public ICommand CmdRefreshProcessList
+        { 
+            get
+            {
+                if (_cmdRefreshProcessList == null)
+                {
+                    _cmdRefreshProcessList = new RelayCommand(x => RefreshProcessList());
+                }
+                return _cmdRefreshProcessList;
+            }
+        }
+
+        private void RefreshProcessList()
+        { 
+
+        }
     }
 }
